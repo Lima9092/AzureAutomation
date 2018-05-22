67b7353fb955a5e1caa163149ece3bd9ccacf6d9
Lee Jolley <leejolley@me.com>
2018-05-22 23:36:49 +0100

    Update

--- ConfigAD.ps1
+++ ConfigAD.ps1
@@ -1,7 +1,12 @@
+$UserName = "xxx"
+$Password = "xxx" 
+$DomUserName = "$DomainName\xxx"
+$DomPassword = "xxx"
+$SafePass = "xxx"
+
 # Configure Active Directory
   $csv = import-csv AzureVMs.csv 
   $csv | foreach-object {
-    $IPAddress = $_.'IPAddress'
     $Type = $_.'Type'
     $DomainName = $_.'DomainName'
     $DNSName = $_.'DNSName'
@@ -11,36 +16,52 @@
     if ($Type -eq "Domain Controller") {
 
       # Set Credentials
-      $UserName="labadmin"
-      $Password="Password@123"| ConvertTo-SecureString -Force -AsPlainText
-      $Credential=New-Object PSCredential($UserName,$Password)
+      $UserName = $UserName
+      $Password = $Password | ConvertTo-SecureString -Force -AsPlainText
+      $Credential = New-Object PSCredential($UserName,$Password)
 
       # Establish Remote PS Session and configure Domain Controller
       $session = New-PSSession -ComputerName $DNSName -Credential $Credential -UseSSL -SessionOption(New-PSsessionOption -SkipCACheck -SkipCNCheck)
         Invoke-command -Session $session {
-          $SafePass="Password@123"| ConvertTo-SecureString -Force -AsPlainText
+          $SafePass = $Using:SafePass | ConvertTo-SecureString -Force -AsPlainText
           Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
           Install-ADDSForest -DomainName $Using:DomainName -InstallDns -SafeModeAdministratorPassword $SafePass -Force
           }
     }
   
-  # Wait for Domain Controllers to reboot
-  Start-Sleep -s 300
+# Wait for Domain Controllers to reboot
+Start-Sleep -s 300
 
     if ($Type -eq "Member Server") {
 
     # Set Credentials
-    $UserName="labadmin"
-    $Password="Password@123"| ConvertTo-SecureString -Force -AsPlainText
+    $UserName = $UserName
+    $Password = $Password | ConvertTo-SecureString -Force -AsPlainText
     $Credential=New-Object PSCredential($UserName,$Password)
-    $DomUserName="$DomainName\labadmin"
-    $DomPassword="Password@123"| ConvertTo-SecureString -Force -AsPlainText
-    $DomCredential=New-Object PSCredential($UserName,$Password)
+    $DomUserName = $DomUserName 
+    $DomPassword = $DomPassword | ConvertTo-SecureString -Force -AsPlainText
+    $DomCredential = New-Object PSCredential($UserName,$Password)
 
      # Establish Remote PS Session, add server/s to domain and restart
         $session = New-PSSession -ComputerName $DNSName -Credential $Credential -UseSSL -SessionOption(New-PSsessionOption -SkipCACheck -SkipCNCheck)
-          Invoke-command -Session $session -script {
+          Invoke-command -Session $session {
              Add-Computer –DomainName $using:DomainName -Credential $Using:DomCredential -Restart –Force
           }       
     }
+  }
+
+# Wait for Servers to reboot
+Start-Sleep -s 300
+
+# Shutdown VMs
+# Configure Active Directory
+  $csv = import-csv AzureVMs.csv 
+  $csv | foreach-object {
+    $DNSName = $_.'DNSName'
+
+     # Establish Remote PS Session and shutdown
+        $session = New-PSSession -ComputerName $DNSName -Credential $Credential -UseSSL -SessionOption(New-PSsessionOption -SkipCACheck -SkipCNCheck)
+          Invoke-command -Session $session {
+             Stop-Computer
+        }       
   }
