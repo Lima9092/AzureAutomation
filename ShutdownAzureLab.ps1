62a0de55e18ad92c2475fac1d5e63e0564d8a1f3
leejolley <39303680+leejolley@users.noreply.github.com>
2018-06-04 23:47:42 +0100

    Delete ShutdownAzureLab.ps1

--- ShutdownAzureLab.ps1
+++
@@ -1,7 +0,0 @@
-$csv = import-csv AzureVMS.csv 
-$csv | foreach-object {
-    $ResourceGroup = $_.'ResourceGroup' 
-    $VMName = $_.'VMName'
-
-    Stop-AzureRmVM -ResourceGroupName $ResourceGroup -Name $VMName -Force
-}
